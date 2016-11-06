00992ae85b928d013895a9b2dbfd310ff2f0119c
shmilee <shmilee.zju@gmail.com>
2016-11-06 22:19:49 +0800

    widgets setting: alsa, battery, weather

--- mywidgets.lua
+++ mywidgets.lua
@@ -5,6 +5,11 @@ local lain = require("lain")
 local widgets = require("widgets")
 local helpers = require("lain.helpers")
 
+-- setting
+local use_battery_bar = false
+local use_volume_bar  = false
+local weather_widget  = 'cn'
+
 -- get device table in path/prefix*
 local function get_device(path,prefix)
     local cmd = 'ls -d ' .. path ..'/' .. prefix .. '* 2>/dev/null | sed "s|' .. path .. '/||"'
@@ -107,151 +112,151 @@ tempwidget  = lain.widgets.temp({
     end
 })
 
--- Battery
+-- Battery, Battery bar
 BAT_table   = get_device('/sys/class/power_supply','BAT')
 baticon = wibox.widget.imagebox(beautiful.bat)
---batwidget = widgets.dualbat({
---    batterys     = BAT_table,
---    followmouse  = true,
---    settings = function()
---        if bat_now.perc == "N/A" then
---            bat_now.icon = beautiful.ac
---            widget:set_markup(" AC ")
---        else
---            bat_perc = tonumber(bat_now.perc)
---            if bat_perc > 50 then
---                widget:set_markup(" " .. bat_now.perc .. "% ")
---                bat_now.icon = beautiful.bat
---            elseif bat_perc > 15 then
---                widget:set_markup(markup("#EB8F8F", bat_now.perc .. "% "))
---                bat_now.icon = beautiful.bat_low
---            else
---                widget:set_markup(markup("#D91E1E", bat_now.perc .. "% "))
---                bat_now.icon = beautiful.bat_no
---            end
---        end
---        if bat_now.ac_status == "1" or bat_now.status == "Charging" then
---            bat_now.icon = beautiful.ac
---        end
---        baticon:set_image(bat_now.icon)
---    end
---})
---batwidget.attach(baticon)
---batwidget.attach(batwidget)
-
--- Battery bar
-batbar = awful.widget.progressbar()
-batbar:set_color(beautiful.fg_normal)
-batbar:set_width(55)
-batbar:set_ticks(true)
-batbar:set_ticks_size(6)
-batbar:set_background_color(beautiful.bg_normal)
-batmargin = wibox.layout.margin(batbar, 2, 7)
-batmargin:set_top(6)
-batmargin:set_bottom(6)
-batupd = widgets.dualbat({
-    batterys     = BAT_table,
-    followmouse  = true,
-    settings = function()
-        if bat_now.perc == "N/A" then
-            bat_now.icon = beautiful.ac
-        else
-            bat_perc = tonumber(bat_now.perc)
-            batbar:set_value(bat_perc / 100)
-            if bat_perc > 50 then
-                batbar:set_color(beautiful.fg_normal)
-                bat_now.icon = beautiful.bat
-            elseif bat_perc > 15 then
-                batbar:set_color("#EB8F8F")
-                bat_now.icon = beautiful.bat_low
+if use_battery_bar then
+    batbar = awful.widget.progressbar()
+    batbar:set_color(beautiful.fg_normal)
+    batbar:set_width(55)
+    batbar:set_ticks(true)
+    batbar:set_ticks_size(6)
+    batbar:set_background_color(beautiful.bg_normal)
+    batmargin = wibox.layout.margin(batbar, 2, 7)
+    batmargin:set_top(6)
+    batmargin:set_bottom(6)
+    batupd = widgets.dualbat({
+        batterys     = BAT_table,
+        followmouse  = true,
+        settings = function()
+            if bat_now.perc == "N/A" then
+                bat_now.icon = beautiful.ac
             else
-                batbar:set_color("#D91E1E")
-                bat_now.icon = beautiful.bat_no
+                bat_perc = tonumber(bat_now.perc)
+                batbar:set_value(bat_perc / 100)
+                if bat_perc > 50 then
+                    batbar:set_color(beautiful.fg_normal)
+                    bat_now.icon = beautiful.bat
+                elseif bat_perc > 15 then
+                    batbar:set_color("#EB8F8F")
+                    bat_now.icon = beautiful.bat_low
+                else
+                    batbar:set_color("#D91E1E")
+                    bat_now.icon = beautiful.bat_no
+                end
             end
+            if bat_now.ac_status == "1" or bat_now.status == "Charging" then
+                bat_now.icon = beautiful.ac
+            end
+            baticon:set_image(bat_now.icon)
         end
-        if bat_now.ac_status == "1" or bat_now.status == "Charging" then
-            bat_now.icon = beautiful.ac
+    })
+    batwidget = wibox.widget.background(batmargin)
+    batwidget:set_bgimage(beautiful.widget_bg)
+    batupd.attach(baticon)
+    batupd.attach(batwidget)
+else
+    batwidget = widgets.dualbat({
+        batterys     = BAT_table,
+        followmouse  = true,
+        settings = function()
+            if bat_now.perc == "N/A" then
+                bat_now.icon = beautiful.ac
+                widget:set_markup(" AC ")
+            else
+                bat_perc = tonumber(bat_now.perc)
+                if bat_perc > 50 then
+                    widget:set_markup(" " .. bat_now.perc .. "% ")
+                    bat_now.icon = beautiful.bat
+                elseif bat_perc > 15 then
+                    widget:set_markup(markup("#EB8F8F", bat_now.perc .. "% "))
+                    bat_now.icon = beautiful.bat_low
+                else
+                    widget:set_markup(markup("#D91E1E", bat_now.perc .. "% "))
+                    bat_now.icon = beautiful.bat_no
+                end
+            end
+            if bat_now.ac_status == "1" or bat_now.status == "Charging" then
+                bat_now.icon = beautiful.ac
+            end
+            baticon:set_image(bat_now.icon)
         end
-        baticon:set_image(bat_now.icon)
-    end
-})
-batwidget = wibox.widget.background(batmargin)
-batwidget:set_bgimage(beautiful.widget_bg)
-batupd.attach(baticon)
-batupd.attach(batwidget)
+    })
+    batwidget.attach(baticon)
+    batwidget.attach(batwidget)
+end
 
--- ALSA volume
+-- ALSA volume, volume bar
 volicon = wibox.widget.imagebox(beautiful.vol)
-volumewidget = lain.widgets.alsa({
-    settings = function()
-        if volume_now.status == "off" then
-            volicon:set_image(beautiful.vol_mute)
-            widget:set_markup(markup("#EB8F8F", volume_now.level .. "% "))
-        elseif tonumber(volume_now.level) == 0 then
-            volicon:set_image(beautiful.vol_no)
-            widget:set_markup(markup("#EB8F8F", volume_now.level .. "% "))
-        elseif tonumber(volume_now.level) <= 50 then
-            volicon:set_image(beautiful.vol_low)
-            widget:set_markup(markup("#7493D2", volume_now.level .. "% "))
-        else
-            volicon:set_image(beautiful.vol)
-            widget:set_markup(markup(beautiful.fg_normal, volume_now.level .. "% "))
-        end
-    end
-})
-volume = {
-    channel = volumewidget.channel,
-    step = "2%",
-    mynotify = function()
-        volumewidget.update()
-        if volume_now.status == "off" then
+if use_volume_bar then
+    volume  = lain.widgets.alsabar({
+        width = 30, ticks = true, ticks_size = 3, step = "2%",
+        followmouse = true,
+        settings = function()
+            if volume_now.status == "off" then
+                volicon:set_image(beautiful.vol_mute)
+            elseif volume_now.level == 0 then
+                volicon:set_image(beautiful.vol_no)
+            elseif volume_now.level <= 50 then
+                volicon:set_image(beautiful.vol_low)
+            else
+                volicon:set_image(beautiful.vol)
+            end
+        end,
+        colors =
+        {
+            background = beautiful.bg_focus,
+            mute       = "#EB8F8F",
+            unmute     = beautiful.fg_normal
+        }
+    })
+    volmargin = wibox.layout.margin(volume.bar, 1, 1)
+    volmargin:set_top(6)
+    volmargin:set_bottom(6)
+    volumewidget = wibox.widget.background(volmargin)
+    volumewidget:set_bgimage(beautiful.widget_bg)
+    function volume.mynotify()
+        volume.notify()
+        if volume._muted then
             os.execute(string.format("volnoti-show -m"))
         else
             os.execute(string.format("volnoti-show %s", volume_now.level))
         end
     end
-}
-
--- ALSA volume bar
---volicon = wibox.widget.imagebox(beautiful.vol)
---volume  = lain.widgets.alsabar({
---    width = 30, ticks = true, ticks_size = 3, step = "2%",
---    followmouse = true,
---    settings = function()
---        if volume_now.status == "off" then
---            volicon:set_image(beautiful.vol_mute)
---        elseif volume_now.level == 0 then
---            volicon:set_image(beautiful.vol_no)
---        elseif volume_now.level <= 50 then
---            volicon:set_image(beautiful.vol_low)
---        else
---            volicon:set_image(beautiful.vol)
---        end
---    end,
---    colors =
---    {
---        background = beautiful.bg_focus,
---        mute       = "#EB8F8F",
---        unmute     = beautiful.fg_normal
---    }
---})
---volmargin = wibox.layout.margin(volume.bar, 1, 1)
---volmargin:set_top(6)
---volmargin:set_bottom(6)
---volumewidget = wibox.widget.background(volmargin)
---volumewidget:set_bgimage(beautiful.widget_bg)
---function volume.mynotify()
---    volume.notify()
---    if volume._muted then
---        os.execute(string.format("volnoti-show -m"))
---    else
---        os.execute(string.format("volnoti-show %s", volume_now.level))
---    end
---end
+else
+    volumewidget = lain.widgets.alsa({
+        settings = function()
+            if volume_now.status == "off" then
+                volicon:set_image(beautiful.vol_mute)
+                widget:set_markup(markup("#EB8F8F", volume_now.level .. "% "))
+            elseif tonumber(volume_now.level) == 0 then
+                volicon:set_image(beautiful.vol_no)
+                widget:set_markup(markup("#EB8F8F", volume_now.level .. "% "))
+            elseif tonumber(volume_now.level) <= 50 then
+                volicon:set_image(beautiful.vol_low)
+                widget:set_markup(markup("#7493D2", volume_now.level .. "% "))
+            else
+                volicon:set_image(beautiful.vol)
+                widget:set_markup(markup(beautiful.fg_normal, volume_now.level .. "% "))
+            end
+        end
+    })
+    volume = {
+        channel = volumewidget.channel,
+        step = "2%",
+        mynotify = function()
+            volumewidget.update()
+            if volume_now.status == "off" then
+                os.execute(string.format("volnoti-show -m"))
+            else
+                os.execute(string.format("volnoti-show %s", volume_now.level))
+            end
+        end
+    }
+end
 
 -- Weather
-weather = 'cn'
-if weather == 'cn' then
+if weather_widget == 'cn' then
     yawn = widgets.cnweather({
         --timeout = 600,            -- 10 min
         --timeout_forecast = 18000, -- 5 hrs
