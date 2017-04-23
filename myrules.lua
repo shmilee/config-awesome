bb50f113afed7f499d23e8373f91cddaeb657acb
shmilee <shmilee.zju@gmail.com>
2017-04-23 14:03:09 +0800

    rule for ristretto

--- myrules.lua
+++ myrules.lua
@@ -1,3 +1,5 @@
+-- https://awesomewm.org/doc/api/classes/client.html
+
 -- additional Rules to apply to new clients
 local myrules = {
     -- web browser videos fullscreen mode
@@ -5,7 +7,7 @@ local myrules = {
         instance = { "plugin-container" },
         class = { "/usr/lib/firefox/plugin-container" },
         role = { "_NET_WM_STATE_FULLSCREEN" },
-    },
+      },
       properties = { floating = true } },
     -- deepin-scrot
     { rule = { class = "Tipswindow.py" },
@@ -17,6 +19,11 @@ local myrules = {
     { rule_any = { instance = {'TM.exe', 'QQ.exe'} },
       properties = { floating = true,
                      border_width = 0, } },
+    -- Ristretto
+    { rule = { class = "Ristretto" },
+      properties = { --maximized = false,
+                     maximized_horizontal = false,
+                     maximized_vertical = false } },
     -- Add titlebars to normal clients and dialogs
     --{ rule_any = {type = { "normal", "dialog" }
     --  }, properties = { titlebars_enabled = true }
