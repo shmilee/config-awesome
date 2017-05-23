8784153f27809e4079e1546e37b63ba8c67f73df
shmilee <shmilee.zju@gmail.com>
2017-05-23 21:55:42 +0800

    conky: graph heigth

--- conky.lua
+++ conky.lua
@@ -67,7 +67,7 @@ end
 net_devices = {'eth0', 'eth1', 'wlan0', 'wlan1', 'docker0', 'ap0'}
 net_text = [[${if_existing /sys/class/net/%s/operstate up}
 ${color green}@%s: ${addr %s} ${hr 1}${color}
-${color green}${downspeedgraph %s 14,90} ${alignr}${upspeedgraph %s 14,90}${color}
+${color green}${downspeedgraph %s 16,90} ${alignr}${upspeedgraph %s 16,90}${color}
 ${font Arrows}i${font}${downspeed %s}/s ${alignr}${upspeed %s}/s${font Arrows}a${font}
 Total ${totaldown %s} ${alignr}Total ${totalup %s}
 ${endif}]]
