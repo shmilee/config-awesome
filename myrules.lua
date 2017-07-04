048382ccd4b098bf79977e099f35e8a308cb6a74
shmilee <shmilee.zju@gmail.com>
2017-07-04 21:58:53 +0800

    group rules

--- myrules.lua
+++ myrules.lua
@@ -7,23 +7,30 @@ local myrules = {
         instance = { "plugin-container" },
         class = { "/usr/lib/firefox/plugin-container" },
         role = { "_NET_WM_STATE_FULLSCREEN" },
-      },
-      properties = { floating = true } },
-    -- deepin-scrot
-    { rule = { class = "Tipswindow.py" },
-      properties = { floating = true } },
-    -- Pidgin
-    { rule = { class = "Pidgin" },
-      properties = { floating = true } },
+      }, properties = { floating = true }},
+    -- other floating clients
+    { rule_any = {
+        class = {
+            "Tipswindow.py", -- deepin-scrot
+            "Pidgin",
+            "netease-cloud-music",
+        },
+      }, properties = { floating = true }},
     -- WineQQ
-    { rule_any = { instance = {'TM.exe', 'QQ.exe'} },
-      properties = { floating = true,
-                     border_width = 0, } },
-    -- Ristretto
-    { rule = { class = "Ristretto" },
-      properties = { --maximized = false,
-                     maximized_horizontal = false,
-                     maximized_vertical = false } },
+    { rule_any = {
+        instance = { 'TM.exe', 'QQ.exe' }
+      }, properties = { floating = true,
+                        border_width = 0 }},
+    -- not maximized
+    { rule_any = {
+        class = {
+            "Ristretto",
+            "netease-cloud-music",
+        },
+      }, properties = {
+          --maximized = false,
+          maximized_horizontal = false,
+          maximized_vertical = false }},
     -- Add titlebars to normal clients and dialogs
     --{ rule_any = {type = { "normal", "dialog" }
     --  }, properties = { titlebars_enabled = true }
