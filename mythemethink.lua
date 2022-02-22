4acd3f4a8c8ca511e4759f0d285e8467fa3081df
shmilee <shmilee.zju@gmail.com>
2022-02-23 01:56:54 +0800

    update away xrandr_menu, mythemethink

--- mythemethink.lua
+++ mythemethink.lua
@@ -10,16 +10,16 @@ local os    = { getenv = os.getenv }
 -- inherit away think theme
 local theme = dofile(away.util.curdir .. "themes/think/theme.lua")
 
-function theme.set_videowall(s, i)
+function theme.get_videowall(s, i)
     if i == 1 then
-        s.videowallpaper = away.wallpaper.get_videowallpaper(s, {
+        return away.wallpaper.get_videowallpaper(s, {
             path = os.getenv("HOME") ..'/视频/Futari.Dake.No.Hanabi.mp4',
             xargs = {'-b -ov -ni -nf -un -s -st -sp -o 0.816'},
             after_prg = 'conky\\s+-c\\s+.*/awesome/conky.lua',
         })
     elseif i == 2 then
         --http://fy4.nsmc.org.cn/portal/cn/theme/FY4A.html
-        s.videowallpaper = away.wallpaper.get_videowallpaper(s, {
+        return away.wallpaper.get_videowallpaper(s, {
             -- 3h, 6h, 12h, 24h, 48h, 72h
             path = 'http://img.nsmc.org.cn/CLOUDIMAGE/FY4A/MTCC/VIDEO/FY4A.disk.24h.mp4',
             --path = 'http://img.nsmc.org.cn/CLOUDIMAGE/FY4A/MTCC/VIDEO/FY4A.china.24h.mp4',
@@ -35,7 +35,7 @@ function theme.set_videowall(s, i)
             timeout = 3600*12,
         })
     elseif i == 3 then
-        s.videowallpaper = away.wallpaper.get_bilivideowallpaper(s, {
+        return away.wallpaper.get_bilivideowallpaper(s, {
             path='https://live.bilibili.com/9196015',
             --choices = {'flv'},
             after_prg = 'conky\\s+-c\\s+.*/awesome/conky.lua',
@@ -47,6 +47,27 @@ end
 
 theme.terminal = "xfce4-terminal"
 local find_icon = away.menu.find_icon
+
+local xrandr_info = [[Monitors: 2
+ 0: +*eDP1 1366/310x768/170+0+0  eDP1
+ 1: +HDMI1 3840/1220x2160/690+1366+0  HDMI1
+]]
+
+function theme.xrandr_menu()
+    return away.xrandr({
+        info = xrandr_info,
+        items = {
+            { menuname="HS-MiTV", dpi=144, complete=true, monitors={
+                { key='eDP1-310x170', scale=1.5 }, -- laptop T450
+                { key='HDMI1-1220x690', scale=1.0 } -- MiTV
+            } },
+            { menuname='Reset', dpi=96, complete=true, monitors={
+                { key='eDP1-310x170', scale=1.0 }, -- laptop T450
+            } },
+        }
+    })
+end
+
 function theme.custommenu()
     return {
         { "终端 (&T)", theme.terminal, find_icon('terminal') },
