2e220441e08026ce5f0dbefec45771b3774a2685
shmilee <shmilee.zju@gmail.com>
2024-09-28 20:51:35 +0800

    update away; unfocus client.opacity=0.95

--- rc.lua
+++ rc.lua
@@ -460,7 +460,7 @@ client.connect_signal("mouse::enter", function(c)
 end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus c.opacity = 1 end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal c.opacity = 0.9 end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal c.opacity = 0.95 end)
 -- }}}
 
 -- autostart programs
